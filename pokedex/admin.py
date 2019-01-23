e38a5aebbc759f80080b18280af67611167d9a7c
Fernando Espinoza <fercho.esp1818@gmail.com>
2019-01-23 00:09:40 -0500

    Created pokedex app models and registered in to Django Admin

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from pokedex.models import Type, Pokemon
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'evolve_from', 'type_1', 'type_2')
+    ordering = ('number', )
+    raw_id_fields = ('evolve_from', 'type_1', 'type_2', )
--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(
+        max_length=15,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Types'
+
+    def __str__(self):
+        return '{name} Type'.format(
+            name=self.name
+        )
+
+
+class Pokemon(models.Model):
+    number = models.PositiveIntegerField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=30
+    )
+    height = models.FloatField(
+        null=True,
+        blank=True
+    )
+    weight = models.FloatField(
+        null=True,
+        blank=True
+    )
+    evolve_from = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="evolve_to",
+        null=True,
+        blank=True
+    )
+    type_1 = models.ForeignKey(
+        Type,
+        on_delete=models.PROTECT,
+        related_name="pokemon_type_1"
+    )
+    type_2 = models.ForeignKey(
+        Type,
+        on_delete=models.PROTECT,
+        related_name="pokemon_type_2",
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Pokemon'
+        verbose_name_plural = 'Pokemon'
+
+    def __str__(self):
+        return '{number}: {name}'.format(
+            number=self.number,
+            name=self.name
+        )
